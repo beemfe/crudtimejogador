9c7a8cb5aceca7c150c0409e407dec58409a198c
beemfe <144623179+beemfe@users.noreply.github.com>
2024-11-18 01:19:04 -0300

    MainActivity.java

--- app/src/main/java/br/edu/fateczl/crudtimejogador/view/MainActivity.java
+++ app/src/main/java/br/edu/fateczl/crudtimejogador/view/MainActivity.java
@@ -1,36 +1,70 @@
 /*
- *@author: Felipe Bernardes Cisilo
+ * @author: Felipe Bernardes Cisilo
  * RA: 1110482413017
  */
+package br.edu.fateczl.crudtimejogador.view;
 
-package br.edu.fateczl.crudtimejogador;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import br.edu.fateczl.crudtimejogador.view.JogadorFragment;
-import br.edu.fateczl.crudtimejogador.view.TimeFragment;
+import br.edu.fateczl.crudtimejogador.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        if (savedInstanceState == null) {
-            loadFragment(new TimeFragment());
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            carregaFragment(bundle);
+        } else {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment, new InicioFragment());
+            fragmentTransaction.commit();
         }
+    }
+
+    private void carregaFragment(Bundle bundle) {
+        String tipo = bundle.getString("tipo");
+        assert tipo != null;
+        if (tipo.equals("time")) {
+            fragment = new TimeFragment();
+        } else {
+            fragment = new JogadorFragment();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("MainActivity", "onCreateOptionsMenu called");
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
@@ -38,19 +72,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.menu_time) {
-            loadFragment(new TimeFragment());
-            return true;
-        } else if (id == R.id.menu_jogador) {
-            loadFragment(new JogadorFragment());
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        Bundle bundle = new Bundle();
+        Intent intent = new Intent(this, MainActivity.class);
 
-    private void loadFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.commit();
+        if (id == R.id.menu_time) {
+            bundle.putString("tipo", "time");
+        } else if (id == R.id.menu_jogador) {
+            bundle.putString("tipo", "jogador");
+        }
+        intent.putExtras(bundle);
+        this.startActivity(intent);
+        this.finish();
+        return true;
     }
 }
